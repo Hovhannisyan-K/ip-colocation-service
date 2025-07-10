98a66333a1affb9bde7be6bc0aa9e155fc257c29
Karen <kar3n.hovhannisyan@gmail.com>
2025-07-10 14:12:43 +0400

    * initialize Spring Boot project (Java 21, Maven, Spring Web starter)

---
+++ src/main/java/com/example/ipcolocationservice/IpColocationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ipcolocationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpColocationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpColocationServiceApplication.class, args);
+    }
+
+}
