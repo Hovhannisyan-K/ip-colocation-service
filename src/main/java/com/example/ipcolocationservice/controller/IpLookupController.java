65535c60f8ac72a901973d283942e61b082a4ff5
Karen <kar3n.hovhannisyan@gmail.com>
2025-07-10 15:25:20 +0400

    * add IP validation to IpLookupController and integrate commons-validator dependency

--- src/main/java/com/example/ipcolocationservice/controller/IpLookupController.java
+++ src/main/java/com/example/ipcolocationservice/controller/IpLookupController.java
@@ -1,8 +1,15 @@
 package com.example.ipcolocationservice.controller;
 
+import com.example.ipcolocationservice.dto.IpInfoResponse;
+import com.example.ipcolocationservice.exceptions.ExternalApiException;
 import com.example.ipcolocationservice.service.IpLookupService;
+import org.apache.commons.validator.routines.InetAddressValidator;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/ip")
@@ -14,5 +21,22 @@ public class IpLookupController {
         this.lookupService = lookupService;
     }
 
+    @GetMapping("/{ip}")
+    public IpInfoResponse getIpInfo(@PathVariable String ip) {
+        if (!isValidIp(ip)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid IP: " + ip);
+        }
+        try {
+            return lookupService.lookup(ip);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        } catch (ExternalApiException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_GATEWAY, e.getMessage());
+        }
+    }
 
+
+    private boolean isValidIp(String ip) {
+        return InetAddressValidator.getInstance().isValid(ip);
+    }
 }
