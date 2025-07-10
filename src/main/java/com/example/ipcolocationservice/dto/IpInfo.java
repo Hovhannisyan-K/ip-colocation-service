b0ba358b8affb12fca1f16533506169f71955744
Karen <kar3n.hovhannisyan@gmail.com>
2025-07-10 14:44:39 +0400

    * add IpInfo DTO to model external API colocation response

---
+++ src/main/java/com/example/ipcolocationservice/dto/IpInfo.java
@@ -0,0 +1,17 @@
+package com.example.ipcolocationservice.dto;
+
+import lombok.Data;
+
+/**
+ * Represents the IP colocation data returned by the external API.
+ */
+@Data
+public class IpInfo {
+    private String ipAddress;
+    private String continent;
+    private String country;
+    private String region;
+    private String city;
+    private double latitude;
+    private double longitude;
+}
